3c2dd6052ac9558588f8bf8c52d10d3bf2b6afe9
SASI CHAVA <88151734+sasikumarchava@users.noreply.github.com>
2025-02-04 12:22:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,7 @@ class SQLQueryGenerator:
         - first_replied_date
         - ticket_create_date
 
-        suppose, if example condition has like this **select created date based on dispose date 2025-01-22**. then consider only created date column only not the all data.
-        suppose, if example condition has like this **select created date based on dispose date 2025-01-22 00:00:00**. then give the data on particular timestamp.
+        suppose, if example condition has like this **select created date based on dispose date 2025-01-22 00:00:00**. then give the data on particular timestamp.then i want query like this **SELECT created_date\nFROM `dev-kapture.demoDataset.assigned_to_resolve_report`\nWHERE DISPOSED_DATE = TIMESTAMP(`2025-01-22 10:10:10`);**
         
         
         Then, ensure the query filters records where the date falls **between 00:00:00 and 23:59:59** of the given date, using **BigQuery TIMESTAMP format**.
